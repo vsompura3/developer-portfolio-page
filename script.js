4b60d981b06c505504a0b61e76270c93ca14c2f6
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-01-18 21:26:55 +0530

    complete v1 with tailwindcss

---
+++ script.js
@@ -0,0 +1,13 @@
+const navToggle = document.getElementById('nav-toggle');
+const nav = document.querySelector('nav');
+
+navToggle.addEventListener('click', function () {
+  const visibility = nav.getAttribute('data-visible');
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    nav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      black: '#000000',
+      sunny: '#FBE850',
+      rosey: '#FE3C72',
+      cement: '#ADADAD',
+      dull: '#2E3038',
+    },
+    fontFamily: {
+      sans: ['Inter', 'Arial', 'Helvetica', 'sans-serif'],
+      serif: ['Prata', 'Arial', 'Helvetica', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
